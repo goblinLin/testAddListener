a10b7ef65732d64a79f3653649b011c3849922ef
Pin ChuehLin <zack@noahgames.com>
2015-10-14 12:39:14 +0800

    加入dispath範例

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@
 -----------------------------------------------------------------------------------------
 
 --[[
-	這個範例解說如何為按鈕加上一個Touch Event Listener
+	這個範例解說如何為按鈕加上一個Touch Event Listener，並說明如何用程式來發出事件
 	Author: Zack Lin
 	Time: 2015/3/13
 ]]
@@ -36,6 +36,7 @@ btn2.y = _VIEWSPACE.HEIGHT / 6 * 3
 --宣告當按鈕觸發touch事件所要執行的內容
 --這種寫法只能供某一個元件使用
 function btn1:touch(e)
+	print( 'btn1:touch' )
 	if (e.phase == "began") then
 		print( "onClick began" );
 		btn1.xScale = 1.2
@@ -51,6 +52,7 @@ end
 
 --這種寫法可讓多個元件共同使用
 local function touch2(e)
+	print( 'touch2' )
 	local target = e.target
 	if (e.phase == "began") then
 		print( "onClick began" );
@@ -68,5 +70,20 @@ end
 --加入偵聽器
 btn1:addEventListener( "touch" )
 btn2:addEventListener("touch",touch2)
+
+--由程式來發動事件
+timer.performWithDelay( 3100, function ( )
+	print( 'click by program' )
+	local event = { name="touch" , phase ="began"} --觸發按下去事件
+	btn1:dispatchEvent( event )
+end )
+
+timer.performWithDelay( 3200, function ( )
+	print( 'click by program' )
+	local event = { name="touch" , target=btn1 , phase ="ended"} --觸發放開事件
+	btn1:dispatchEvent( event )
+end )
+
+
 
 
